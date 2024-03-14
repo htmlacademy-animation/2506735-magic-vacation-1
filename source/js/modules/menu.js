dad5c5ce3317c4ecfa6804391aa6efafaed618fd
Ilya Kirsanov <kirsanovia@yandex.ru>
2024-03-14 23:03:34 +0400

    module1-task3: add screen fill effect

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -2,6 +2,7 @@ export default () => {
   let header = document.querySelector(`.js-header`);
   let menuToggler = document.querySelector(`.js-menu-toggler`);
   let menuLinks = document.querySelectorAll(`.js-menu-link`);
+  let fillEffect = document.querySelector(`.screen-fill-effect`);
 
   if (menuToggler) {
     menuToggler.addEventListener(`click`, function () {
@@ -16,7 +17,22 @@ export default () => {
   }
 
   for (let i = 0; i < menuLinks.length; i++) {
-    menuLinks[i].addEventListener(`click`, function () {
+    const link = menuLinks[i];
+    const pageTo = link.getAttribute(`href`);
+
+    link.addEventListener(`click`, function (e) {
+      const pageFrom = window.location.hash;
+
+      if (pageTo === `#prizes` && pageFrom === `#story`) {
+        e.preventDefault();
+        fillEffect.classList.add(`active`);
+      }
+
+      setTimeout(() => {
+        window.location.href = pageTo;
+        fillEffect.classList.remove(`active`);
+      }, 500);
+
       if (window.innerWidth < 1025) {
         header.classList.remove(`page-header--menu-opened`);
         document.body.classList.remove(`menu-opened`);
